a73dd0dbadc49625f4ef1655f817156c15253821
B-Deprez <bruno.r.deprez@gmail.com>
2025-07-31 13:57:19 +0200

    insurance data class

---
+++ src/data/graph_data.py
@@ -0,0 +1,44 @@
+# LOAD MODULES
+# Standard library
+from typing import Union, Optional, Dict, Any
+
+# Proprietary
+
+# Third party
+import pickle
+
+# DATA LOADING FUNCTION
+def load_data_insurance(
+    data_path: str, 
+):
+    with open(data_path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+def add_none_line(data, number_node_types: int = 1):
+    for i in range(1, number_node_types + 1):
+        num_features = data[i].shape[1]
+        data[i].loc['NA'] = tuple([None] * num_features)
+        data[i] = data[i].astype(float)
+
+class NetworkData:
+    def __init__(self, data, number_node_types: int = 1):
+        self.data = add_none_line(data, number_node_types)
+        self.number_node_types = number_node_types
+        # Ensure self.data contains entry at self.data[1].loc['NA']
+        # Added for debugging purposes
+        if 'NA' not in self.data[1].index:
+            raise ValueError("Addition of 'NA' line failed. 'NA' not found in data[1].index.")
+
+    def get_graph(self):
+        return self.data[0]
+
+    def get_data(self, node_type: int = 1):
+        if node_type > self.number_node_types:
+            raise ValueError(f"Node type {node_type} exceeds number of node types {self.number_node_types}.")
+        return self.data[node_type]
+    
+    def get_label(self, node_type: int = 1):
+        if node_type > self.number_node_types*2:
+            raise ValueError(f"Node type {node_type} exceeds number of node types {self.number_node_types}.")
+        return self.data[node_type].get('label', None)
